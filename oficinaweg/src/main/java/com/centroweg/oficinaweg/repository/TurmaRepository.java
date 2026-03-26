61c3cfab98fd9185e484e24f11c99472c4b16ad7
jonathan_uber <jonathanuber121207@gmail.com>
2026-03-26 14:39:09 -0300

    feat: cria repositorio de turma que tinha esquecido

---
+++ oficinaweg/src/main/java/com/centroweg/oficinaweg/repository/TurmaRepository.java
@@ -0,0 +1,9 @@
+package com.centroweg.oficinaweg.repository;
+
+import com.centroweg.oficinaweg.model.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+}
