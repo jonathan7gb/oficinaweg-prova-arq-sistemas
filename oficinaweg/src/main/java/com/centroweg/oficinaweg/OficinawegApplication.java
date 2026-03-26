7b1338003e30425b39ac47eb625175338601d1b3
jonathan_uber <jonathanuber121207@gmail.com>
2026-03-26 14:14:16 -0300

    feat: initial commit

---
+++ oficinaweg/src/main/java/com/centroweg/oficinaweg/OficinawegApplication.java
@@ -0,0 +1,13 @@
+package com.centroweg.oficinaweg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinawegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinawegApplication.class, args);
+	}
+
+}
---
+++ oficinaweg/src/test/java/com/centroweg/oficinaweg/OficinawegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroweg.oficinaweg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinawegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
