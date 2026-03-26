7263e8d9e418c10f01314c2c6058b3fa97d8a55e
jonathan_uber <jonathanuber121207@gmail.com>
2026-03-26 14:39:22 -0300

    feat: cria repositorio de turma que tinha esquecido

--- oficinaweg/src/main/java/com/centroweg/oficinaweg/repository/OsRepository.java
+++ oficinaweg/src/main/java/com/centroweg/oficinaweg/repository/OsRepository.java
@@ -3,9 +3,11 @@ package com.centroweg.oficinaweg.repository;
 import com.centroweg.oficinaweg.model.OrdemServico;
 import com.centroweg.oficinaweg.model.StatusOS;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OsRepository extends JpaRepository<OrdemServico, Long> {
     List<OrdemServico> findByStatus(StatusOS status);
 }
